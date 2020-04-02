b0998a035af70c4156ee59f366db8f2e5b6bedf9
jasjenand <jasjenand@gmail.com>
2020-04-02 16:00:19 -0400

    Updated with collab code

--- asst_2/main.js
+++ asst_2/main.js
@@ -6,13 +6,27 @@ const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
-
-for (let i = 1; i = 5; i++) {
-    newImage.setAttribute('pic')
+for (let i = 1; i < 6; i++) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', 'images/pic' + i + '.jpg')
+    thumbBar.appendChild(newImage);
+    
+    newImage.onclick = function() {
+        displayedImage.getAttribute('src')
+        displayedImage.setAttribute('src', newImage.src)
+    }
+    
 
 }
 
 /* Wiring up the Darken/Lighten button */
+function myButton() {
+    var click = document.createElement("onClick");
+    click.innerHTML = "Dark";
+    document.body.appendChild(click);
+    //https://www.w3schools.com/jsref/met_document_createelement.asp source
+    
+    //btn.onclick =
+}
+
+//Source: https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers/onclick
