d011c637466371d983f47aca7085ffb0a3e7ca95
jasjenand <jasjenand@gmail.com>
2020-04-02 00:30:26 -0400

    Starting p2

--- asst_2/main.js
+++ asst_2/main.js
@@ -10,4 +10,9 @@ const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
 thumbBar.appendChild(newImage);
 
+for (let i = 1; i = 5; i++) {
+    newImage.setAttribute('pic')
+
+}
+
 /* Wiring up the Darken/Lighten button */
